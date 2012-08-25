7124011590570f0746d571472c567a118682ccdc
Matthew Hughes <matt@roostercatgames.com>
2012-08-25 02:02:56 -0300

    fixed bsp generation to be truely recursive, also this fixes wonky-ness

--- src/bsp.py
+++ src/bsp.py
@@ -62,6 +62,35 @@ class box:
 		height = self.height
 		return "box["+`x`+','+`y`+','+`int(x+width)`+','+`int(y+height)`+"]"
 
+def split(box,minSize, depth = 5):
+	# make sure we didn't go too deep
+	if depth is 0:
+		return [ box ] # list of just this box
+	# let's assume we _need_ to split this
+	vertical = 1
+	horizontal = 2
+	direction = random.randint(1,2) # vertical or horizontal?
+	#
+	#
+	if direction is vertical:
+		if box.height < minSize * 2:
+			direction = horizontal
+	if direction is horizontal:
+		if box.width < minSize * 2:
+			if box.height < minSize * 2:
+				return [ box ] # return a list containing just this box
+			direction = vertical
+	#
+	# Now that size has been verified, perform split:
+	if direction is vertical:
+		subBoxes = box.splitV(random.randint(minSize,box.height-minSize))
+	else: # horizontal
+		subBoxes = box.splitH(random.randint(minSize,box.width-minSize))
+	#
+	if len(subBoxes) is 2:
+		return split(subBoxes[0], minSize, depth - 1) + split(subBoxes[1], minSize, depth - 1)
+
+
 if __name__ == '__main__':
 	scr = curses.initscr()
 	curses.curs_set(0)
@@ -75,37 +104,8 @@ if __name__ == '__main__':
 	width = maxYX[1] - minYX[1]-5
 	height = maxYX[0] - minYX[0]-5
 	boxes = [ box(3,3,width,height) ]
-	recurs=3
-	for i in range(0,recurs):
-		nboxes =  []
-		for b in boxes:
-			#nboxes.append(b)
-			rand = random.random()
-			if rand > 0.5:
-				if b.width < 8:
-					nboxes.append(b)
-					break
-				ysplit = int(random.uniform(4,b.width-4))
-				bA,bB = b.splitV(ysplit)
-				if bA:
-					nboxes.append(bA)
-					nboxes.append(bB)
-				else:
-					nboxes.append(b)
-			else:
-				if b.height < 8:
-					nboxes.append(b)
-					break;
-				xsplit = int(random.uniform(4,b.height-4))
-				#print xsplit
-				bA,bB = b.splitH(xsplit)	
-				if bA:
-					nboxes.append(bA)
-					nboxes.append(bB)
-				else:
-					nboxes.append(b)
-		boxes = nboxes
 	print "DIM ",width,height
+	boxes = split(boxes[0],4,3)
 	for b in boxes:
 		b.draw()
 		print "box: ",`b.x`,`b.y`,`b.width`,`b.height`
